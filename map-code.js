16916145a919318582f35deffd071e02f92e4d7e
méch frazier <41482472+mefrazi2@users.noreply.github.com>
2021-11-29 08:18:40 -0600

    updated md to js duh

--- map-code.js
+++ map-code.js
@@ -1,4 +1,4 @@
-```<head>
+<head>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/>
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 </head>
